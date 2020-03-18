99d2bcd4d7c1d282e38533a37b39de973b00c0d8
Zachary Nawrocki <zacharynawrocki@a.local>
2020-03-17 21:59:45 -0400

    began add friend/remove friend, consistent syntax, spelling/grammar

---
+++ SocialNetwork/client/src/components/profile/ProfileFriend.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Moment from 'react-moment';
+import moment from 'moment';
+
+const ProfileFriends = ({
+}) => (
+);
+
+ProfileFriends.propTypes = {
+  friend: PropTypes.object.isRequired
+};
+
+export default ProfileFriends;
