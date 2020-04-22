70302a891e68082bbea5f0c10ce80746840eb221
pgravelin <pygravelin@gmail.com>
2020-04-22 14:06:30 -0400

    Add simple logistic regression model

---
+++ SocialNetwork/recommender/PreProcessPosts.py
@@ -0,0 +1,33 @@
+import re
+from string import punctuation 
+from nltk.corpus import stopwords 
+from nltk.stem.wordnet import WordNetLemmatizer
+
+class PreProcessPosts:
+    def __init__(self):
+        self._stopwords = set(stopwords.words('english') + list(punctuation))
+    
+    def processPosts(self, list_of_posts):
+        processedPosts=[]
+        labels=[]
+        for post in list_of_posts:
+            processedPosts.append(self._processPost(post["text"]))
+            labels.append(1 if post["label"] == "Positive" else 0)
+        return (processedPosts,labels)
+
+    def _processPost(self, post):
+        post = post.lower() # convert text to lower-case
+        post = re.sub('[^a-zA-Z]', ' ', post)  # Remove punctuations and numbers
+        post = re.sub(r"\s+[a-zA-Z]\s+", ' ', post) # Single character removal
+        post = re.sub(r'\s+', ' ', post) # Removing multiple spaces
+        post = ' '.join(self.normalize(list(post.split(' '))))
+        return post
+
+        # Convert to base words
+    def normalize(self, post_tokens):
+        lem = WordNetLemmatizer()
+        normalized_post = []
+        for word in post_tokens:
+            normalized_text = lem.lemmatize(word,'v')
+            normalized_post.append(normalized_text)
+        return normalized_post
--- SocialNetwork/recommender/sentiment_classifier.py
+++ SocialNetwork/recommender/sentiment_classifier.py
@@ -1,9 +1,13 @@
 import sys
 import csv
 import nltk
+import pandas as pd
+import numpy as np
 from PreProcessPosts import PreProcessPosts
-
-from numpy import array
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
 
 def buildDataset(filename):
     dataset  = [] 
@@ -28,3 +32,29 @@ if __name__ == '__main__':
     # Pre-process (tokenize and denoise) post texts
     postProcessor = PreProcessPosts()
     X, y = postProcessor.processPosts(dataset)
+
+    filepath_dict = {'yelp':   'data/yelp_labelled.txt',
+                    'amazon': 'data/amazon_cells_labelled.txt',
+                    'imdb':   'data/imdb_labelled.txt'}
+
+    sentences_train, sentences_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.25, random_state=1000)
+
+    vectorizer = CountVectorizer()
+    vectorizer.fit(sentences_train)
+
+    X_train = vectorizer.transform(sentences_train)
+    X_test  = vectorizer.transform(sentences_test)
+
+    classifier = LogisticRegression(C=0.5,max_iter=500)
+    classifier.fit(X_train, y_train)
+    score = classifier.score(X_test, y_test)
+
+    # Enter test sentences here.
+    X_real_data = [
+        "It's not the same as it used to be... I think about my killing myself often",
+        "I just won the lottery and my life has really never been better."
+    ]
+
+    X_real_data = vectorizer.transform(X_real_data)
+    print(classifier.predict(X_real_data))
